d84581b32fc0101bffc7104ecbf36fc396447f9b
Philipp Fromme <philippfromme@outlook.com>
2021-04-01 15:35:46 +0200

    feat: UI

--- src/components/TemplatesPluginModal.js
+++ src/components/TemplatesPluginModal.js
@@ -48,15 +48,23 @@ export default class TemplatesPluginModal extends Component {
       onSubmit
     } = this.props;
 
-    const {
+    let {
       [ ids.name ]: name,
       [ ids.template ]: template
     } = values;
 
-    onSubmit({
+    template = {
       ...JSON.parse(template),
       name
+    };
+
+    template.properties = template.properties.filter((property, index) => {
+      return values[ 'property_' + index ];
     });
+
+    debugger
+
+    onSubmit(template);
 
     setSubmitting(false);
 
@@ -83,6 +91,10 @@ export default class TemplatesPluginModal extends Component {
       [ ids.template ]: JSON.stringify(template, null, 2)
     };
 
+    template.properties.forEach((property, index) => {
+      initialValues[ 'property_' + index ] = true;
+    });
+
     return (
       <Modal className={ css.Modal } onClose={ onClose }>
 
@@ -94,11 +106,43 @@ export default class TemplatesPluginModal extends Component {
 
               <Modal.Body>
                 <fieldset className={ css.fieldset }>
+                  <legend className={ css.legend }>
+                    General
+                  </legend>
                   <div className="fields">
                     <TextField id={ ids.name } label="Name" />
-                    <TextArea id={ ids.template } label="Template" disabled={ true } />
                   </div>
                 </fieldset>
+
+                <fieldset className={ css.fieldset }>
+                  <legend className={ css.legend }>
+                    Properties
+                  </legend>
+                  <p className={ css.description }>
+                    Select properties for template.
+                  </p>
+                  <div className="fields">
+                    {
+                      template.properties.map((property, index) => {
+                        return <PropertyCheckbox
+                          key={ 'property_' + index }
+                          id={ 'property_' + index }
+                          name={ property.binding.name }
+                          value={ property.value } />;
+                      })
+                    }
+                  </div>
+                </fieldset>
+                {/*
+                <fieldset className={ css.fieldset }>
+                  <legend className={ css.legend }>
+                    Preview
+                  </legend>
+                  <div className="fields">
+                    <TextArea id={ ids.template } disabled={ true } />
+                  </div>
+                </fieldset> */}
+
               </Modal.Body>
 
               <Modal.Footer>
@@ -142,8 +186,33 @@ function TextArea(props) {
 
   return (
     <div className="form-group">
-      <label htmlFor={ id }>{ label }</label>
+      {
+        label ? <label htmlFor={ id }>{ label }</label> : null
+      }
       <Field type="text" as="textarea" name={ id } id={ id } disabled={ disabled } rows={ rows } className="form-control" />
+    </div>
+  );
+}
+
+function PropertyCheckbox(props) {
+  const {
+    id,
+    name,
+    value
+  } = props;
+
+  return (
+    <div className="form-group">
+      <div className="custom-control custom-checkbox">
+        <Field type="checkbox" name={ id } id={ id } className="custom-control-input" />
+        <label htmlFor={ id } className="custom-control-label" style={ { display: 'flex', 'align-items': 'center' } }>
+          <pre style={ { display: 'inline', margin: 0 } }>{ name }</pre>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="001616">
+            <polygon fill="#909090" fill-rule="evenodd" points="8 3 8 7 15 7 15 9 8 9 8 13 2 8" />
+          </svg>
+          <pre style={ { display: 'inline', margin: 0 } }>{ value.toString() }</pre>
+        </label>
+      </div>
     </div>
   );
 }
